2143288b0f634615bab06eaeafc89da55ef1dc8a
vibhitha <125384259+vibhitha@users.noreply.github.com>
2024-10-23 11:39:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,4 +49,5 @@ def predict():
         return render_template('result.html', result=result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
